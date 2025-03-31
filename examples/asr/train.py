4b590d13a38fda3b1012d0f75bd18db73d7b4813
IlyaKalinovskiy <kalinovskiy.ai@gmail.com>
2025-03-31 14:50:47 +0300

    Final task solution

--- examples/asr/train.py
+++ examples/asr/train.py
@@ -1,5 +1,6 @@
 import typing as tp
 
+from collections import defaultdict
 from dataclasses import dataclass
 from pathlib import Path
 
@@ -8,7 +9,6 @@ import torch
 import torchaudio
 import pytorch_lightning as pl
 
-from nemo.collections.asr.modules import AudioToMelSpectrogramPreprocessor
 from pytorch_lightning import Callback
 from torch import nn
 from torch.nn import functional as F
@@ -32,6 +32,7 @@ from speechflow.data_server.loader import DataLoader
 from speechflow.io import (
     AudioChunk,
     Config,
+    check_path,
     construct_file_list,
     json_load_from_file,
     split_file_list,
@@ -59,8 +60,8 @@ from speechflow.utils.tensor_utils import run_rnn_on_padded_sequence
 class ASRDataSample(DataSample):
     audio_chunk: AudioChunk = None
     waveform: torch.Tensor = None
-    transcription: str = None
     mel: torch.Tensor = None
+    transcription: str = None
     tokens: torch.Tensor = None
 
 
@@ -81,6 +82,7 @@ class ASRTarget(TrainData):
 @dataclass
 class ASRForwardInput(TrainData):
     waveform: torch.Tensor = None
+    waveform_lengths: torch.Tensor = None
     mel: torch.Tensor = None
     mel_lengths: torch.Tensor = None
 
@@ -104,7 +106,7 @@ class DSParser(BaseDSParser):
         metadata = {
             "file_path": file_path,
             "audio_chunk": audio_chunk,
-            "transcription": transcription,
+            "transcription": transcription.strip(),
         }
         return [metadata]
 
@@ -150,9 +152,9 @@ class MelProcessor(BaseDSProcessor):
 
         self._mel_module = None
 
-        self.logging_params(self.get_config_from_locals())
-
     def init(self):
+        from nemo.collections.asr.modules import AudioToMelSpectrogramPreprocessor
+
         super().init()
         self._mel_module = AudioToMelSpectrogramPreprocessor(**self._mel_cfg)
         self._mel_module.eval()
@@ -160,8 +162,6 @@ class MelProcessor(BaseDSProcessor):
     @PipeRegistry.registry(inputs={"audio_chunk"}, outputs={"mel"})
     @lazy_initialization
     def process(self, ds: ASRDataSample) -> ASRDataSample:
-        ds = super().process(ds)
-
         waveform = ds.audio_chunk.waveform
         waveform = torch.from_numpy(waveform).unsqueeze(0)
         waveform_length = torch.LongTensor([waveform.shape[-1]])
@@ -172,34 +172,80 @@ class MelProcessor(BaseDSProcessor):
         return ds.to_numpy()
 
 
-class Tokenizer(BaseDSProcessor):
-    def __init__(self, alphabet_path: str = "alphabet.json", blank_symbol: str = "_"):
+class MorzeTokenizer(BaseDSProcessor):
+    @check_path(assert_file_exists=True)
+    def __init__(
+        self,
+        alphabet_path: Path,
+        blank_symbol: str = "_",
+        use_morze_alphabet: bool = False,
+        use_morze_correction: bool = False,
+    ):
         super().__init__()
         self.blank_symbol = blank_symbol
 
-        self.alphabet = json_load_from_file(alphabet_path)
-        self.alphabet[self.blank_symbol] = None
+        self.alphabet = {self.blank_symbol: None}
+        self.alphabet.update(json_load_from_file(alphabet_path))
 
         self.symbol_to_id = {s: i for i, s in enumerate(self.alphabet)}
         self.id_to_symbol = {i: s for i, s in enumerate(self.alphabet)}
+
+        self.morze_alphabet = [blank_symbol, "|", "-", ".", " "]
+        self.morze_symbol_to_id = {s: i for i, s in enumerate(self.morze_alphabet)}
+        self.morze_id_to_symbol = {i: s for i, s in enumerate(self.morze_alphabet)}
+
+        self.use_morze_alphabet = use_morze_alphabet
+        self.use_morze_correction = use_morze_correction
         self.logging_params(self.get_config_from_locals())
 
     @property
-    def labels(self) -> tp.List:
-        return list(self.id_to_symbol.values())
+    def labels(self) -> tp.List[str]:
+        if not self.use_morze_alphabet:
+            return list(self.id_to_symbol.values())
+        else:
+            return list(self.morze_id_to_symbol.values())
 
     @property
     def blank_index(self) -> int:
-        return self.symbol_to_id[self.blank_symbol]
+        if not self.use_morze_alphabet:
+            return self.symbol_to_id[self.blank_symbol]
+        else:
+            return self.morze_symbol_to_id[self.blank_symbol]
 
     @PipeRegistry.registry(inputs={"transcription"}, outputs={"tokens"})
     def process(self, ds: ASRDataSample) -> ASRDataSample:
-        ds = super().process(ds)
+        if not self.use_morze_alphabet:
+            ds.tokens = np.asarray(
+                [self.symbol_to_id[t] for t in ds.transcription], dtype=np.int64
+            )
+        else:
+            morze_transcription = []
+            for s in ds.transcription:
+                for m in self.alphabet[s] if self.alphabet[s] is not None else s:
+                    morze_transcription.append(m)
+                morze_transcription.append("|")
+            morze_transcription.pop()
 
-        ds.tokens = np.asarray(
-            [self.symbol_to_id[t] for t in ds.transcription], dtype=np.int64
-        )
+            ds.tokens = np.asarray(
+                [self.morze_symbol_to_id[t] for t in morze_transcription], dtype=np.int64
+            )
         return ds
+
+    def morze_to_char(self, morze_word: str) -> str:
+        if morze_word == "":
+            return ""
+        elif morze_word == " ":
+            return " "
+        else:
+            try:
+                return {v: k for k, v in self.alphabet.items()}[morze_word]
+            except KeyError:
+                if self.use_morze_correction:
+                    for k, v in self.alphabet.items():
+                        if v and torchaudio.functional.edit_distance(v, morze_word) == 1:
+                            return k
+
+                return ""
 
 
 class ASRCollate(BaseCollate):
@@ -210,9 +256,9 @@ class ASRCollate(BaseCollate):
         collated.waveform, collated.waveform_lengths = collate_sequence(
             batch, "waveform", pad_values=0
         )
-        collated.mel, collated.mel_lengths = collate_sequence(batch, "mel", pad_values=0)
+        # collated.mel, collated.mel_lengths = collate_sequence(batch, "mel", pad_values=0)
         collated.tokens, collated.token_lengths = collate_sequence(
-            batch, "tokens", pad_values=-1
+            batch, "tokens", pad_values=0
         )
 
         collated.transcription = [ds.transcription for ds in batch]  # type: ignore
@@ -225,10 +271,8 @@ class ASRCollate(BaseCollate):
 
 
 class ASRModelParams(BaseTorchModelParams):
-    mel_dim: int = 80
-    inner_dim: int = 256
-    output_dim: int = 512
-    with_rnn: bool = False
+    output_dim: int = 256
+    with_rnn: bool = True
 
 
 class ASRModel(BaseTorchModel):
@@ -241,36 +285,40 @@ class ASRModel(BaseTorchModel):
     ):
         super().__init__(ASRModelParams.create(cfg, strict_init))
 
-        bundle = torchaudio.pipelines.HUBERT_BASE
+        bundle = torchaudio.pipelines.WAV2VEC2_ASR_LARGE_LV60K_960H
         self.model = bundle.get_model()
-        self.model_dim = self.model.encoder.transformer.layer_norm.weight.shape[0]
+
+        inner_dim = self.model.model.aux.in_features
+        self.proj = nn.Linear(inner_dim, self.params.output_dim)
 
         if self.params.with_rnn:
             self.rnn = nn.GRU(
-                self.model_dim,
-                self.params.inner_dim,
+                inner_dim,
+                inner_dim // 2,
                 batch_first=True,
                 bidirectional=True,
             )
-            self.proj = nn.Linear(2 * self.params.inner_dim, self.params.output_dim)
         else:
             self.rnn = None
-            self.proj = nn.Linear(self.model_dim, self.params.output_dim)
 
     def forward(self, inputs: ASRForwardInput) -> ASRForwardOutput:
-        x, _ = self.model(inputs.waveform)
-        x_lens = (inputs.mel_lengths * x.shape[1]).long()
+        x_lens = (inputs.waveform_lengths * inputs.waveform.shape[1]).long()
+        x, _ = self.model.model.feature_extractor(inputs.waveform, x_lens)
+
+        x = self.model.model.encoder(x)
 
         if self.params.with_rnn:
-            after_rnn = run_rnn_on_padded_sequence(self.rnn, x, x_lens)
+            x_lens = (inputs.waveform_lengths * x.shape[1]).long()
+            y = run_rnn_on_padded_sequence(self.rnn, x, x_lens)
         else:
-            after_rnn = x
+            y = x
 
-        y = self.proj(after_rnn)
+        z = self.proj(y)
+
         return ASRForwardOutput(
-            logits=y,
-            log_probs=F.log_softmax(y, dim=-1),
-            output_lengths=inputs.mel_lengths,
+            logits=z,
+            log_probs=F.log_softmax(z, dim=-1),
+            output_lengths=inputs.waveform_lengths,
         )
 
 
@@ -311,7 +359,7 @@ class Criterion(BaseCriterion):
         target_lens = (target.token_lengths * target.tokens.shape[1]).ceil().long()
         ctc_loss = torch.nn.functional.ctc_loss(
             output.log_probs.transpose(0, 1),
-            target.tokens,
+            target.tokens.int(),
             input_lens,
             target_lens,
             self.blank_index,
@@ -322,12 +370,13 @@ class Criterion(BaseCriterion):
 
 
 class ValidationMetrics(Callback):
-    def __init__(self, labels, blank_index: int = 0):
-        self.decoder = GreedyCTCDecoder(labels, blank_index)
-        self.greedy_result = None
+    def __init__(self, tokenizer: MorzeTokenizer):
+        self.tokenizer = tokenizer
+        self.decoder = GreedyCTCDecoder(tokenizer.labels, tokenizer.blank_index)
+        self.metrics = None
 
     def on_validation_start(self, pl_module: pl.LightningModule, *args):
-        self.greedy_result = []
+        self.metrics = defaultdict(list)
 
     def on_validation_batch_end(
         self,
@@ -343,14 +392,35 @@ class ValidationMetrics(Callback):
         for i in range(outputs.logits.shape[0]):
             actual_transcript = targets.transcription[i]
             greedy_result = self.decoder(outputs.logits[i])
-            greedy_wer = torchaudio.functional.edit_distance(
-                actual_transcript, greedy_result
-            ) / len(actual_transcript)
-            self.greedy_result.append(greedy_wer)
 
-    def on_validation_epoch_end(self, pl_module: pl.LightningModule, *args):
-        mean_wer = np.asarray(self.greedy_result).mean()
-        print("WER:", mean_wer)
+            if self.tokenizer.use_morze_alphabet:
+                chars = []
+                for word in greedy_result.split("|"):
+                    chars.append(self.tokenizer.morze_to_char(word))
+
+                predict_transcript = "".join(chars)
+            else:
+                predict_transcript = greedy_result
+
+            greedy_wer = torchaudio.functional.edit_distance(
+                actual_transcript, predict_transcript
+            )
+            self.metrics["WER"].append(greedy_wer)
+
+            if batch_idx == 0:
+                pl_module.logger.experiment.add_text(
+                    f"transcription_{i} [actual]|[predict]",
+                    f"[{actual_transcript}]|[{predict_transcript}]",
+                    global_step=pl_module.global_step,
+                )
+
+    def on_validation_epoch_end(
+        self,
+        trainer: pl.Trainer,
+        pl_module: pl.LightningModule,
+        *args,
+    ):
+        pl_module.log("WER", np.asarray(self.metrics["WER"]).mean(), prog_bar=True)
 
 
 class GreedyCTCDecoder(torch.nn.Module):
@@ -360,13 +430,6 @@ class GreedyCTCDecoder(torch.nn.Module):
         self.blank_index = blank_index
 
     def forward(self, emission: torch.Tensor) -> str:
-        """Given a sequence emission over labels, get the best path
-        Args:
-          emission (Tensor): Logit tensors. Shape `[num_seq, num_label]`.
-
-        Returns:
-          List[str]: The resulting transcript
-        """
         indices = torch.argmax(emission, dim=-1)  # [num_seq,]
         indices = torch.unique_consecutive(indices, dim=-1)
         indices = [i for i in indices if i != self.blank_index]
@@ -383,8 +446,8 @@ def create_data_pipline(subsets, alphabet_path):
     parser = DSParser()
     preprocessing = [
         SignalProcessor().process,
-        MelProcessor().process,
-        Tokenizer(alphabet_path=alphabet_path).process,
+        # MelProcessor().process,
+        MorzeTokenizer(alphabet_path=alphabet_path).process,
     ]
     collate = ASRCollate(relative_lengths=True)
     sampler = RandomSampler()
@@ -393,7 +456,12 @@ def create_data_pipline(subsets, alphabet_path):
     )
 
 
-def train(experiment_path: str, loaders: tp.Dict[str, DataLoader]):
+def train(
+    experiment_path: str,
+    loaders: tp.Dict[str, DataLoader],
+    accelerator: str = "cpu",
+    max_epochs: int = 30,
+):
     # set seed
     pl.seed_everything(1234)
 
@@ -403,9 +471,9 @@ def train(experiment_path: str, loaders: tp.Dict[str, DataLoader]):
     batch_processor = BatchProcessor()
 
     # create dnn model
-    tokenizer = Tokenizer(dl_train.client.find_info("alphabet_path"))
+    tokenizer = MorzeTokenizer(dl_train.client.find_info("alphabet_path"))
 
-    model_params = ASRModelParams(output_dim=len(tokenizer.alphabet))
+    model_params = ASRModelParams(output_dim=len(tokenizer.labels))
     model = ASRModel(model_params)
 
     # create criterion
@@ -414,8 +482,8 @@ def train(experiment_path: str, loaders: tp.Dict[str, DataLoader]):
     # create optimizer
     optimizer = Optimizer(
         model,
-        method={"type": "Adam", "weight_decay": 1.0e-6},
-        lr_scheduler={"type": "ConstLR", "lr_max": 0.001},
+        method={"type": "AdamW", "weight_decay": 1.0e-6},
+        lr_scheduler={"type": "WarmupInvRsqrtLR", "lr_max": 0.001},
     )
 
     # create experiment saver
@@ -435,23 +503,24 @@ def train(experiment_path: str, loaders: tp.Dict[str, DataLoader]):
     # create trainer callbacks
     checkpoint_callback_cfg = Config(
         {
-            "monitor": "Epoch",
-            "mode": "max",
+            "monitor": "ctc_loss/valid",
+            "filename": "asr_checkpoint_{epoch}_{step}_{WER:.4f}",
+            "mode": "min",
             "save_top_k": 1,
             "every_n_epochs": 1,
         }
     )
     callbacks = [
-        pl.callbacks.LearningRateMonitor(logging_interval="epoch"),
+        pl.callbacks.LearningRateMonitor(logging_interval="step"),
         saver.get_checkpoint_callback(cfg=checkpoint_callback_cfg),
-        ValidationMetrics(tokenizer.labels, tokenizer.blank_index),
+        ValidationMetrics(tokenizer),
     ]
 
     # create trainer
     trainer = pl.Trainer(
-        accelerator="cpu",
+        accelerator=accelerator,
         devices=1,
-        max_epochs=10,
+        max_epochs=max_epochs,
         default_root_dir=experiment_path,
         callbacks=callbacks,
     )
@@ -464,8 +533,8 @@ def train(experiment_path: str, loaders: tp.Dict[str, DataLoader]):
 
 
 if __name__ == "__main__":
-    _dataset_path = r"M:\Ilya\JustAI/cw_dataset_internship_rus_20k"
-    _alphabet_path = r"M:\Ilya\JustAI/alphabet.json"
+    _dataset_path = r"cw_dataset_internship_rus_20k"
+    _alphabet_path = r"alphabet.json"
     _expr_path = "_logs/test_asr"
     _file_ext = ".txt"
 
@@ -473,19 +542,18 @@ if __name__ == "__main__":
         subsets=["train", "valid"], alphabet_path=_alphabet_path
     )
 
-    _flist = construct_file_list(_dataset_path, _file_ext, with_subfolders=True)[:16]
+    _flist = construct_file_list(_dataset_path, _file_ext, with_subfolders=True)
     _flist_train, _flist_valid = split_file_list(_flist, ratio=0.8)
-    _flist_train, _flist_valid = _flist, _flist
+
     if 1:  # TRAINING
         set_verbose_logging()
 
         with LoggingServer.ctx(_expr_path):
             with init_data_loader(
-                loader_params=LoaderParams(batch_size=8, non_stop=True),
+                loader_params=LoaderParams(batch_size=16, non_stop=True),
                 data_pipeline=_data_pipeline,
                 flist_by_subsets={"train": _flist_train, "valid": _flist_valid},
-                n_processes=1,
-                n_gpus=0,
+                n_processes=8,
             ) as _loaders:
                 train(_expr_path, _loaders)
     else:  # EVALUATION
@@ -500,14 +568,24 @@ if __name__ == "__main__":
         _model.load_state_dict(_checkpoint["state_dict"])
 
         _tokenizer = Tokenizer(_alphabet_path)
-        _greedy_decoder = GreedyCTCDecoder(_tokenizer.labels, _tokenizer.blank_index)
+        _greedy_decoder = GreedyCTCDecoder(_tokenizer)
 
-        _metadata = _data_pipeline["valid"].dataset_parser.reader(Path(_flist[0]))
+        _metadata = _data_pipeline["valid"].dataset_parser.reader(Path(_flist[-1]))
         _batch = _data_pipeline["valid"].metadata_to_batch(_metadata)
 
         with torch.inference_mode():
             _model_input, _, _ = BatchProcessor()(_batch)
             _predict: ASRForwardOutput = _model(_model_input)
 
-        transcription = _greedy_decoder(_predict.logits[0])
-        print("decode:", transcription)
+        _greedy_result = _greedy_decoder(_predict.logits[0])
+
+        if _tokenizer.use_morze_alphabet:
+            chars = []
+            for word in _greedy_result.split("|"):
+                chars.append(_tokenizer.morze_to_char(word))
+
+            _predict_transcript = "".join(chars)
+        else:
+            _predict_transcript = _greedy_result
+
+        print("decode:", _predict_transcript)
